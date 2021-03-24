f442db6c2c52e5aeaa090fde26c0834424d012f5
Edz <edznan.jusufovic@outlook.com>
2021-03-24 23:52:52 +0100

    init bootstrap and leaflet

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var mymap = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/edznan/cj3sos1ne001h2rp6xpcl2rmb?access_token=pk.eyJ1IjoiZWR6bmFuIiwiYSI6ImNqem1tdmMyZTA1am4zbm55MGUyYzFobXMifQ.zasVy07OkGlOyFZLURha5g', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+})
